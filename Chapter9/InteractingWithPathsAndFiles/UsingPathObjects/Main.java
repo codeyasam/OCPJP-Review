43e422f2ab75b11136430dd858d3c4c16b2540e8
Emmanuel Yasa <emmanuel.yasa@orangeandbronze.com>
2018-11-29 17:12:43 +0800

    Reviewing Chapter 9

--- Chapter9/InteractingWithPathsAndFiles/UsingPathObjects/Main.java
+++ Chapter9/InteractingWithPathsAndFiles/UsingPathObjects/Main.java
@@ -47,3 +47,48 @@ class PathFilePathTest {
     }
     
 }
+
+/* Checking Path Type with isAbsolute() and toAbsolutePath()
+*
+*/
+
+class PathType {
+    
+    public static void main(String[] args) {
+        Path path1 = Paths.get("C:\\birds\\egret.txt");
+        System.out.println("Path1 is Absolute?" + path1.isAbsolute());
+        System.out.println("Absolute Path1: " + path1.toAbsolutePath());
+        
+        Path path2 = Paths.get("birds/condor.txt");
+        System.out.println("Path2 is Absolute? " + path2.isAbsolute());
+        System.out.println("Absolute Path2 " + path2.toAbsolutePath());
+    }
+    
+}
+
+/* Creating new path with subpath() 
+*
+*/
+
+class PathSubPath {
+    
+    public static void main(String[] args) {
+        Path path = Paths.get("/mammal/carnivore/racoon.image");
+        System.out.println("Path is: " + path);
+        
+        System.out.println("Subpath from 0 to 3 is: " + path.subpath(0,3));
+        System.out.println("Subpath from 1 to 3 is: " + path.subpath(1,3));
+        System.out.println("Subpath from 1 to 2 is: " + path.subpath(1,2));
+            
+        // 0 to 4 or 1 to 1 will throw an exception at runtime
+    }
+    
+}
+
+/* Deriving a Path with relativize()
+*
+*/
+
+class PathRelativize {
+
+}
